640dd9552b8f704e017dd5e75e4a7bd9c2726892
245548212@qq.com <245548212@qq.com>
2018-06-02 17:56:07 +0800

    #[tao] 增加微服务配置文件及启动代码

--- eureka-server/src/main/java/com/eureka/server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/eureka/server/EurekaServerApplication.java
@@ -6,8 +6,7 @@ package com.eureka.server;
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
-
-        System.out.println("123");
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
 
     }
 
