b0329ff9b7f8a5cb8da27136e4ed4c54c23313b1
245548212@qq.com <245548212@qq.com>
2018-07-10 23:43:05 +0800

    增加 spring security 安全认证服务

--- spring-security-server/src/main/java/com/security/client/SecurityClientApplication.java
+++ spring-security-server/src/main/java/com/security/client/SecurityClientApplication.java
@@ -2,11 +2,13 @@ package com.security.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Create by dt on 2018/7/10
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class SecurityClientApplication {
 
     public static void main(String[] args) {
