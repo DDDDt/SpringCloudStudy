f77e55570a135f6528ce334d8360c3c9953ee1cd
245548212@qq.com <245548212@qq.com>
2018-07-16 23:54:29 +0800

    增加oauth2安全认证配置

--- spring-oauth-server/src/main/java/com/oauth/server/OauthSeverApplication.java
+++ spring-oauth-server/src/main/java/com/oauth/server/OauthSeverApplication.java
@@ -3,12 +3,14 @@ package com.oauth.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 /**
  * Create by dt on 2018/7/16
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableResourceServer
 public class OauthSeverApplication {
 
     public static void main(String[] args) {
