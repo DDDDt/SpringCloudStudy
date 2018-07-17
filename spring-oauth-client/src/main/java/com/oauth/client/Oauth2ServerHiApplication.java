a127c1ddd91904cb7465bd6f843cf848d9230f50
245548212@qq.com <245548212@qq.com>
2018-07-17 23:03:09 +0800

    修改pom文件

---
+++ spring-oauth-client/src/main/java/com/oauth/client/Oauth2ServerHiApplication.java
@@ -0,0 +1,19 @@
+package com.oauth.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Create by dt on 2018/7/17
+ */
+@SpringBootApplication
+public class Oauth2ServerHiApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Oauth2ServerHiApplication.class);
+
+    }
+
+
+}
